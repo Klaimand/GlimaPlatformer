5d41335ee2ce772b1aac532dfb30b69c5f823dbf
Clément Pagnier <clempagnier@yahoo.fr>
2020-02-18 10:18:07 +0100

    Test de develop

--- GlimaPlatformer/Assets/KLD_Scripts/PlayerController2D.cs
+++ GlimaPlatformer/Assets/KLD_Scripts/PlayerController2D.cs
@@ -6,7 +6,7 @@ public class PlayerController2D : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-
+    //test starfoulah
     [Header("Movement")]
     public float moveSpeed;
     private float xAxis;
