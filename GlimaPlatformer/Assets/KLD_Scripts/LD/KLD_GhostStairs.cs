41e5b26e3f320ea09280c478fd58fafb2dc980fe
Clément Pagnier <clempagnier@yahoo.fr>
2020-03-26 18:36:29 +0100

    Avancements sur le travail des ghoststairs La position du joueur relative a la droite de l'escalier est maintenant prise en compte. Le saut ne fonctionne pas correctement Fonction checkGhost a retaff

--- GlimaPlatformer/Assets/KLD_Scripts/LD/KLD_GhostStairs.cs
+++ GlimaPlatformer/Assets/KLD_Scripts/LD/KLD_GhostStairs.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class KLD_GhostStairs : MonoBehaviour
 {
     BoxCollider2D collider;
+    CapsuleCollider2D playercollider;
+    Rigidbody2D playerrb;
 
     float yAxis;
     [SerializeField]
     float axisThreshold;
     [SerializeField]
     bool toTheLeft;
+    [SerializeField]
+    private float offset;
     bool enabled;
 
     PlayerController2D controller;
@@ -31,6 +35,8 @@ public class KLD_GhostStairs : MonoBehaviour
         origin = transform.GetChild(0);
         origin2 = transform.GetChild(1);
         controller = GameObject.Find("Player").GetComponent<PlayerController2D>();
+        playercollider = GameObject.Find("Player").GetComponent<CapsuleCollider2D>();
+        playerrb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         player = controller.transform;
     }
 
@@ -41,8 +47,62 @@ public class KLD_GhostStairs : MonoBehaviour
         checkGhost();
     }
 
+
     void checkGhost()
     {
+        Vector3 thisVector = transform.GetChild(1).position - transform.GetChild(0).position;
+        Debug.DrawLine(transform.GetChild(1).position, transform.GetChild(0).position, Color.blue);
+
+        Vector3 thisVectorOffset = (transform.GetChild(1).position + Vector3.up * offset) - (transform.GetChild(0).position + Vector3.up * offset);
+        Debug.DrawLine(transform.GetChild(0).position + Vector3.up * offset, transform.GetChild(0).position + Vector3.up * offset + thisVector, Color.green);
+
+        Vector3 stairToPlayerVector = player.position - transform.GetChild(0).position;
+        
+        float xp = thisVector.x * stairToPlayerVector.y - thisVector.y * stairToPlayerVector.x;
+        float xo = thisVectorOffset.x * stairToPlayerVector.y - thisVectorOffset.y * stairToPlayerVector.x;
+
+        if (xp > 0)
+        {
+            //print("above");
+            if (controller.getGroundStatus() && !controller.getStairsStatus()) {
+                if (yAxis > axisThreshold)
+                {
+                    enabled = true;
+                }
+                else
+                {
+                    enabled = false;
+                }
+            }
+            else if (!controller.getGroundStatus())
+            {
+                if (xo > 0)
+                {
+                    enabled = true;
+                }
+                else
+                {
+                    enabled = false;
+                }
+                
+            }
+        }
+
+        else if (xp < 0)
+        {
+            //print("below");
+            enabled = false;
+        }
+
+        else
+        {
+            //print("same line");
+            enabled = false;
+        }
+
+        collider.enabled = enabled;
+
+        //y = ax + b
 
     }
 
