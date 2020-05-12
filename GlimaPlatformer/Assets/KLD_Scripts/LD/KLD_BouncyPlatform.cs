134a59c2239ba9cfa1332c62258d7ed03eb4d1f7
Clément Pagnier <clempagnier@yahoo.fr>
2020-05-12 22:22:22 +0200

    [MERGEABLE] Nouvelles bouncy

--- GlimaPlatformer/Assets/KLD_Scripts/LD/KLD_BouncyPlatform.cs
+++ GlimaPlatformer/Assets/KLD_Scripts/LD/KLD_BouncyPlatform.cs
@@ -13,7 +13,8 @@ public class KLD_BouncyPlatform : MonoBehaviour
     BoxCollider2D thisCollider;
 
     [SerializeField]
-    private float bounciness, minVel, maxVel, bouncinessAddedWhenButtonIsPressed;
+    private float mminVel = 10f, ooutputVel = 25f;
+    //private float bounciness, minVel, outPutVel, bouncinessAddedWhenButtonIsPressed;
     
     private void Awake()
     {
@@ -37,16 +38,21 @@ public class KLD_BouncyPlatform : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            /*
             float bouncinessToAdd = Input.GetButton("Fire1") ? bouncinessAddedWhenButtonIsPressed : 0f;
             playerRb.velocity = new Vector2(playerRb.velocity.x, Mathf.Min(-playerRb.velocity.y * (bounciness + bouncinessToAdd), maxVel));
             events.InvokeBouncyPlatformJump();
-            controller.SetLastJumpIsBounce(true); //not working...
+            controller.SetLastJumpIsBounce(true);
+            */
+            playerRb.velocity = new Vector2(playerRb.velocity.x, ooutputVel);
+            events.InvokeBouncyPlatformJump();
+            controller.SetLastJumpIsBounce(true);
         }
     }
 
     void checkTrigger ()
     {
-        if (player.position.y > transform.position.y && Mathf.Abs(playerRb.velocity.y) > minVel && !controller.cantMove)
+        if (player.position.y > transform.position.y && Mathf.Abs(playerRb.velocity.y) > mminVel && !controller.cantMove)
         {
             thisCollider.isTrigger = true;
         }
