7dca60ca595cd08293cda5a4e9fbbf7a15525c6c
Clément Pagnier <clempagnier@yahoo.fr>
2020-05-18 17:01:49 +0200

    Debut du taff sur l'audiomanager

---
+++ GlimaPlatformer/Assets/KLD_Scripts/Audio/KLD_AudioManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public AudioMixerGroup group;
+
+    [Range(0f, 1f)]
+    public float volume = 0.7f;
+    [Range(0.5f, 1.5f)]
+    public float pitch = 1f;
+
+    [Range(0f, 0.5f)]
+    public float randomVolume = 0f;
+    [Range(0f, 0.5f)]
+    public float randomPitch = 0f;
+
+    private AudioSource source;
+
+    public void SetSource (AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+        source.outputAudioMixerGroup = group;
+    }
+
+    public void Play ()
+    {
+        source.volume = volume * (1 + Random.Range(-randomVolume/2f, randomVolume/2f));
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.Play();
+    }
+
+}
+
+
+public class KLD_AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    Sound[] sounds;
+
+    private void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _go.transform.parent = transform;
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+    }
+
+    public void PlaySound (string _name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.name == _name)
+            {
+                sound.Play();
+                return;
+            }
+        }
+
+        Debug.LogWarning("No found sound '" + _name + "'");
+    }
+    
+}
---
+++ GlimaPlatformer/Assets/KLD_Scripts/Audio/KLD_PlayerAudioInst.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_PlayerAudioInst : MonoBehaviour
+{
+    private KLD_AudioManager audioManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = GameObject.Find("AudioManager").GetComponent<KLD_AudioManager>();
+    }
+
+    public void PlaySound (string soundName)
+    {
+        audioManager.PlaySound(soundName);
+    }
+
+}
