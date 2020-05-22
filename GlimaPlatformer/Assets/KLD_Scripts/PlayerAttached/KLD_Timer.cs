ab7f8e7263722217f9d93839361cd0b66bf901c4
Clément Pagnier <clempagnier@yahoo.fr>
2020-05-22 14:28:57 +0200

    Debut du systeme de segment

---
+++ GlimaPlatformer/Assets/KLD_Scripts/PlayerAttached/KLD_Timer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_Timer : MonoBehaviour
+{
+    public List<float> segmentTimes = new List<float>();
+    public float totalTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addFrameTime();
+    }
+
+    void addFrameTime ()
+    {
+        totalTime += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("SegmentCheckpoint"))
+        {
+            changeSegment();
+        }
+    }
+
+    void changeSegment ()
+    {
+        segmentTimes.Add(totalTime);
+    }
+
+}
