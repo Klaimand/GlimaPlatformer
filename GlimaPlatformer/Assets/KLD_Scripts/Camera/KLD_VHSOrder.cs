7d465201f3070185be0f50774b79ae5adff732c9
Clément Pagnier <clempagnier@yahoo.fr>
2020-06-01 11:51:28 +0200

    On rigole on rigole mais on voit aps le fond du bol

---
+++ GlimaPlatformer/Assets/KLD_Scripts/Camera/KLD_VHSOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KLD_VHSOrder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().sortingOrder = 800;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
